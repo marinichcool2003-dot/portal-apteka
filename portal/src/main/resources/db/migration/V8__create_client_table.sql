36dc1846eec7f09ff19068e2528f60a3f51272bb
Marinich Daniil <marinichcool2003@gmail.com>
2026-05-08 20:02:18 +0300

    Update V8__create_client_table.sql

--- portal/src/main/resources/db/migration/V8__create_client_table.sql
+++ portal/src/main/resources/db/migration/V8__create_client_table.sql
@@ -1,10 +1,16 @@
+DROP TABLE IF EXISTS client_roles;
 DROP TABLE IF EXISTS client;
 CREATE TABLE IF NOT EXISTS client(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     login VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     full_name VARCHAR(255) NOT NULL,
-    role client_role NOT NULL DEFAULT 'USER',
     group_id INT NOT NULL,
     avatar_url VARCHAR(255)
+);
+
+CREATE TABLE client_roles (
+    client_id UUID NOT NULL,
+    role client_role NOT NULL DEFAULT 'USER',
+    CONSTRAINT fk_client_roles FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE
 );
