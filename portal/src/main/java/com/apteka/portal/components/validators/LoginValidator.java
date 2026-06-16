4a694e80bb2b9c9e95567d4b348c4fe8092bf950
Marinich Daniil <marinichcool2003@gmail.com>
2026-06-16 15:59:51 +0300

    1

--- portal/src/main/java/com/apteka/portal/components/validators/LoginValidator.java
+++ portal/src/main/java/com/apteka/portal/components/validators/LoginValidator.java
@@ -8,23 +8,27 @@ import com.apteka.portal.exceptions.InvalidLoginException;
 @Component
 public class LoginValidator {
     public String getCleanLogin(String login) {
+        if (login == null) {
+            throw new InvalidLoginException("Логин не может быть пустым!");
+        }
+        String cleanLogin = login.replaceAll("\\s", "");
 
-        if (!StringUtils.hasText(login)) {
+        if (!StringUtils.hasText(cleanLogin)) {
             throw new InvalidLoginException("Логин не может быть пустым!");
         }
 
-        if (login.length() > 50) {
+        if (cleanLogin.length() > 50) {
             throw new InvalidLoginException("Логин не может быть более 50 символов!");
         }
 
-        if (!login.matches("^[a-zA-Z0-9_.-]+$")) {
-            throw new InvalidLoginException("Логин может содержать только английские буквы, цифры и символы (_ . -)");
+        if (!cleanLogin.matches("^[a-zA-Z0-9_.-@]+$")) {
+            throw new InvalidLoginException("Логин может содержать только английские буквы, цифры и символы (_ . - @)");
         }
 
-        if (!login.endsWith("@farmp.ru")) {
+        if (!cleanLogin.endsWith("@farmp.ru")) {
             throw new InvalidLoginException("Логин должен содержать домен!");
         }
 
-        return login.replaceAll("\\s", "");
+        return cleanLogin;
     }
 }
